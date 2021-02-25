5cc588ca3a7fcc55ca46e2aeba7005a156d0c662
Kris1100 <bobr.200@yandex.ru>
2021-02-25 21:29:28 +0300

    Добавлен класс окружеость

--- redactor/redactor/circle.cpp
+++ redactor/redactor/circle.cpp
@@ -1,1 +1,52 @@
 #include "circle.h"
+
+
+
+circle::circle(double, double, double) const {
+	set_center(x, y);
+	set_r(r);
+}
+
+
+friend circle::ostream& operator<<(ostream&, const rational&) {
+	out << << "Координаты центра (" << c._x << ", " << c._y << ")" << endl << "Радиус: " << c._z << endl;
+	return out;
+}
+friend circle::istream& operator>>(istream&, rational&)const {
+	double x, y, r;
+	in >> x >> y >> r;
+	p.set_center(x, y);
+	p.set_r(r);
+	return in;
+}
+void circle::equation(double, double, double) {
+	set_center(a, b);
+	set_r(r);
+}
+
+void circle::set_center(double, double) {
+	_p.set_x(x);
+	_p.set_y(y);
+}
+
+double circle::get_r() const {
+	return _r;
+}
+
+double circle::get_d() const {
+	return _r * 2;
+}
+void circle::set_r(double) {
+	_r = r;
+}
+
+void circle::quarter() const {
+	if ((_p.get_x() - _r) > 0 && (_p.get_y() - _r) > 0)cout << "1";
+	if ((_p.get_x() + _r) < 0 && (_p.get_y() - _r) > 0)cout << "2";
+	if ((_p.get_x() + _r) < 0 && (_p.get_y() + _r) < 0)cout << "3";
+	if ((_p.get_x() - _r) > 0 && (_p.get_y() + _r) < 0)cout << "4";
+}
+
+double length() const {
+	return 2 * pi * _r;
+}
--- redactor/redactor/circle.h
+++ redactor/redactor/circle.h
@@ -1,5 +1,33 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include "point.h"
+using namespace std;
 class circle
 {
-};
+public:
+	//Перезагрузка вывода
+	friend ostream& operator<<(ostream& out, const circle& c);
+	//Перезагрузка ввода
+	friend istream& operator>>(istream& in, circle& p)const;
+	//Возвращает диаметр
+	double get_d() const;
+	//Конструктор окружности
+	circle(point p, double r = 1) const;
+	//Задание через уравнение !!НЕДОДЕЛАНО!!
+	void equation(double a, double b, double r);
+	//Сеттер центра
+	void set_center(double x, double y);
+	//Возвращает радиус
+	double get_r()const;
+	//Сеттер радиуса
+	void set_r(double r);
+	//Четверть, если окружность полностью в ней
+	void quarter() const;
+	//Возвращает длину окружности
+	double length() const;
 
+private:
+	point _p;
+	double _r;
+};
