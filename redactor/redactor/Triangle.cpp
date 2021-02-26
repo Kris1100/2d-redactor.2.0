108bff9dee0ad26ac72d5f137de8663e89432e91
alonmichu <alon.michu@gmail.com>
2021-02-26 23:51:16 +0300

    an improved version of triangle class

---
+++ redactor/redactor/Triangle.cpp
@@ -0,0 +1,72 @@
+#include "Triangle.h"
+
+triangle::triangle()
+{
+	point* vert = new point[3];
+	set_point_array(vert);
+	delete[]vert;
+	for (int i = 0; i < 3; i++)
+		abc_apexes.push_back(vertex[i]);
+	// mne ne nravitsya, peredelayu
+}
+
+void triangle::set_line_und_angles()
+{
+	//tut doljen byt' cod s pryamoy, no yeyo (ne yego) escho net
+}
+
+void triangle::define_kind()
+{
+	triangle abc;
+	point a = abc.abc_apexes[0];
+	point b = abc.abc_apexes[1];
+	point c = abc.abc_apexes[2];
+	double dist_ab = sqrt( (a.get_x() - b.get_x()) * (a.get_x() - b.get_x()) 
+		                 + (a.get_y() - b.get_y()) * (a.get_y() - b.get_y()) );
+	double dist_bc = sqrt( (b.get_x() - c.get_x()) * (c.get_x() - b.get_x())
+		                 + (c.get_y() - b.get_y()) * (c.get_y() - b.get_y()) );
+	double dist_ac = sqrt( (a.get_x() - c.get_x()) * (a.get_x() - c.get_x())
+		                 + (a.get_y() - c.get_y()) * (a.get_y() - c.get_y()) );
+	if (dist_ab + dist_bc < dist_ac || dist_ab + dist_ac < dist_bc ||
+		dist_ac + dist_bc < dist_ab)
+	{
+		cout << "doesn't exist" << endl;
+		return;
+	}
+	if (dist_ab == dist_bc == dist_ac)
+		cout << "ravnostoronniy" << endl;
+	else
+		if (dist_ab == dist_bc || dist_ab == dist_ac || dist_ac == dist_bc)
+			cout << "ravnobedrenniy" << endl;
+	if (dist_ab * dist_ab + dist_ac * dist_ac == dist_bc * dist_bc ||
+		dist_ab * dist_ab + dist_bc * dist_bc == dist_ac * dist_ac ||
+		dist_bc * dist_bc + dist_ac * dist_ac == dist_ab * dist_ab)
+		cout << "pramougolniy" << endl;
+	//nado dobavit' tupo- i ostrougolniye, kogda poyavitsa pramaya
+}
+
+double triangle::radius_inside()
+{
+	triangle abc;
+	double square = abc.area();
+	double perimetr = abc.perimetr();
+	// hochu destructor
+	return 2 * square / perimetr;
+}
+
+double triangle::radius_outside()
+{
+	triangle abc;
+	double square = abc.area();
+	square *= 4;
+	point a = abc.abc_apexes[0];
+	point b = abc.abc_apexes[1];
+	point c = abc.abc_apexes[2];
+	double dist_ab = sqrt( (a.get_x() - b.get_x()) * (a.get_x() - b.get_x())
+		                 + (a.get_y() - b.get_y()) * (a.get_y() - b.get_y()) );
+	double dist_bc = sqrt( (b.get_x() - c.get_x()) * (c.get_x() - b.get_x())
+		                 + (c.get_y() - b.get_y()) * (c.get_y() - b.get_y()) );
+	double dist_ac = sqrt( (a.get_x() - c.get_x()) * (a.get_x() - c.get_x())
+		                 + (a.get_y() - c.get_y()) * (a.get_y() - c.get_y()) );
+	return dist_ab * dist_bc * dist_ac / square;
+}
--- redactor/redactor/Triangle.h
+++ redactor/redactor/Triangle.h
@@ -1,1 +1,23 @@
 #pragma once
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include"point.h"
+#include"polygon.h"
+using namespace std;
+
+class triangle : public polygon
+{
+public:
+	triangle();                                                                // constructor
+	void set_line_und_angles();                                                // zadat' cherez 2 ugla i pryamuyu
+	void define_kind();                                                        // opredelit' vid
+	//perimetr i ploschad' nasleduyutsa ot polygona, zrya pisala
+	// dlya bissectrisi, mediani, i tp nujna pramaya
+	double radius_inside();                                                    // radius vpisannoy okrugnosti
+	double radius_outside();                                                   // radius opisannoy
+	//ostalas' trigonometriya
+	
+private:
+	vector<point> abc_apexes;
+};
