c3aea6117ddb2fe63cc305fd5ac0b234bace1f77
Kris1100 <bobr.200@yandex.ru>
2021-03-02 06:29:55 +0300

    Исправлены некоторые ошибки

--- redactor/redactor/circle.cpp
+++ redactor/redactor/circle.cpp
@@ -1,28 +1,38 @@
 #include "circle.h"
-circle::circle(double, double, double) const {
-	set_center(x, y);
+
+circle::circle(double x, double y, double r) {
+	point* p;
+	p->set_x(x);
+	p->set_y(y);
+	set_center(p);
 	set_r(r);
 }
- circle::ostream& operator<<(ostream&, const rational&) {
-	out << << "Координаты центра (" << c._x << ", " << c._y << ")" << endl << "Радиус: " << c._z << endl;
+ostream& operator<<(ostream& out, const circle& c) {
+	out << "Координаты центра (" << c._p.get_x << ", " << c._p.get_y << ")" << endl << "Радиус: " << c._r << endl;
 	return out;
 }
- circle::istream& operator>>(istream&, rational&)const {
+istream& operator>>(istream& in, circle& c) {
 	double x, y, r;
 	in >> x >> y >> r;
-	p.set_center(x, y);
-	p.set_r(r);
+	point* p;
+	p->set_x(x);
+	p->set_y(y);
+	c.set_center(p);
+	c.set_r(r);
 	return in;
 }
-void circle::equation(double, double, double) {
-	set_center(a, b);
+void circle::equation(double a, double b, double r) {
+	point* p;
+	p->set_x(a);
+	p->set_y(b);
+	set_center(p);
 	set_r(r);
 }
 
-void circle::set_center(double, double) {
-	_p.set_x(x);
-	_p.set_y(y);
+void circle::set_center(point* p) {
+	_p = p;
 }
+
 
 double circle::get_r() const {
 	return _r;
@@ -31,7 +41,7 @@ double circle::get_r() const {
 double circle::get_d() const {
 	return _r * 2;
 }
-void circle::set_r(double) {
+void circle::set_r(double r) {
 	_r = r;
 }
 
@@ -42,6 +52,12 @@ void circle::quarter() const {
 	if ((_p.get_x() - _r) > 0 && (_p.get_y() + _r) < 0)cout << "4";
 }
 
-double length() const {
-	return 2 * pi * _r;
+
+void circle::intersection() const {
+	if ((_r ^ 2 - _p.get_x ^ 2) > 0)cout << "(0, " << sqrt(_r ^ 2 - _p.get_x ^ 2) << ")  ";
+	if ((_r ^ 2 - _p.get_y ^ 2) > 0)cout << "(" << sqrt(_r ^ 2 - _p.get_y ^ 2) << " ,0)  ";
+}
+
+double circle::length() const {
+	return 2 * M_PI * _r;
 }
--- redactor/redactor/circle.h
+++ redactor/redactor/circle.h
@@ -9,25 +9,28 @@ public:
 	//Перезагрузка вывода
 	friend ostream& operator<<(ostream& out, const circle& c);
 	//Перезагрузка ввода
-	friend istream& operator>>(istream& in, circle& p)const;
+	friend istream& operator>>(istream& in, circle& p);
 	//Возвращает диаметр
 	double get_d() const;
 	//Конструктор окружности
-	circle(point p, double r = 1) const;
+	circle(double x = 0.0, double y = 0.0, double r = 1.0);
 	//Задание через уравнение !!НЕДОДЕЛАНО!!
-	void equation(double a, double b, double r);
+	void equation(double a = 0.0, double b = 0.0, double r = 1.0);
 	//Сеттер центра
-	void set_center(double x, double y);
+	void set_center(point* p);
 	//Возвращает радиус
 	double get_r()const;
 	//Сеттер радиуса
-	void set_r(double r);
+	void set_r(double r = 1.0);
 	//Четверть, если окружность полностью в ней
 	void quarter() const;
 	//Возвращает длину окружности
 	double length() const;
+	//
+	void intersection() const;
+
 
 private:
-	point _p;
+	point* _p;
 	double _r;
 };
