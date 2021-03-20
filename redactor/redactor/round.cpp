e87d493dbcb45e2b7ac661a694efc7e26379b445
Kris1100 <bobr.200@yandex.ru>
2021-03-20 17:51:15 +0300

    Добавлено наследование круга

--- redactor/redactor/round.cpp
+++ redactor/redactor/round.cpp
@@ -1,26 +1,27 @@
 #include "round.h"
 const double PI = 3.141592653589793238463;
-my_round::my_round(double x, double y, double r) {
-	_p = new point(x, y);
 
+my_round::my_round(double x, double y, double r)
+{
+	_p = new point(x, y);
 	set_r(r);
 }
 
-ostream& operator<<(ostream& out, const my_round& c) {
-	out << "Координаты центра (" << c._p->get_x() << ", " << c._p->get_y() << ")" << endl << "Радиус: " << c._r << endl;
+ostream& operator<<(ostream& out, const my_round& c)
+{
+	out << "Координаты центра (" << c._p->get_x() << ", "
+		<< c._p->get_y() << ")" << endl << "Радиус: " << c._r << endl;
 	return out;
 }
-istream& operator>>(istream& in, my_round& c) {
+
+istream& operator>>(istream& in, my_round& c)
+{
 	double x, y, r;
 	cout << "Введите координаты центра и радиус: ";
 	in >> x >> y >> r;
 	c._	p = new point(x, y);
 	c.set_r(r);
 	return in;
-}
-
-double my_round::get_square() const {
-	return (2 * PI * get_r());
 }
 
 void circle::quarter() const {
@@ -31,10 +32,13 @@ void circle::quarter() const {
 	else cout << "Круг не лежит полностью ни в какой четверти" << endl;
 }
 
+double my_round::get_square() const
+{
+	return (2 * PI * get_r());
+}
 
-
-double my_round::get_square_of_segment(double a) const {
+double my_round::get_square_of_segment(double a) const
+{
 	return ((a / 2) * get_r() * get_r());
 }
 	
-
--- redactor/redactor/round.h
+++ redactor/redactor/round.h
@@ -5,20 +5,25 @@
 #include "point.h"
 #include <circle.h>
 using namespace std;
+<<<<<<< HEAD
 class my_round : public circle
+=======
+
+class my_round
+>>>>>>> 7fd4a1c70be8e07ed46fb135d0319a788675f418
 {
 public:
 	//Перезагрузка вывода
 	friend ostream& operator<<(ostream& out, const my_round& c);
 	//Перезагрузка ввода
 	friend istream& operator>>(istream& in, my_round& p);
-	//Возвращает диаметр
 	//Конструктор окружности
 	my_round(double x = 0.0, double y = 0.0, double r = 1.0);
 	//Возвращает площадь круга
 	double get_square() const;
 	//Возвращает площадь сегмента, аргумент - угол в радианах
 	double get_square_of_segment(double a = 1) const;
+
 private:
 	point* _p;
 	double _r;
