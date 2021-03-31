4996408b1851c529ad27e3c4d08adecd376f396b
alonmichu <alon.michu@gmail.com>
2021-03-31 21:40:04 +0300

    code rewiev

--- redactor/redactor/segment.cpp
+++ redactor/redactor/segment.cpp
@@ -1,15 +1,19 @@
 #include "segment.h"
+
 void segment::set_extm_points(point& p1, point& p2)
 {
 	p1_ = p1;
 	p2_ = p2;
 }
-double segment::len()
+
+double segment::len()//не должна ли быть константной?
 {
 	return sqrt( ((p1_.get_x() - p2_.get_x()) * (p1_.get_x() - p2_.get_x()))
 			   + ((p1_.get_y() - p2_.get_y()) * (p1_.get_y() - p2_.get_y())) );
 }
-point segment::middle() {
+
+point segment::middle()
+{
 	double x = (p1_.get_x() + p2_.get_x()) / 2;
 	double y = (p1_.get_y() + p2_.get_y()) / 2;
 	return point(x, y);
