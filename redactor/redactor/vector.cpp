db231f0eb8ffe19ec2614fe6ffbf3c00bc4a19e9
alonmichu <alon.michu@gmail.com>
2021-03-31 21:47:16 +0300

    code rewiev

--- redactor/redactor/vector.cpp
+++ redactor/redactor/vector.cpp
@@ -6,11 +6,11 @@ myvector::myvector(point& begin, point& end)
 	set_coord(end.get_x() - begin.get_x(), end.get_y() - begin.get_y());
 }
 
-myvector::~myvector() 
+myvector::~myvector() //перенести в заголовочный. деструктор наследуется, но вызывать его надо виртуально
 {
 }
 
-void myvector::set_coord(double x, double y)
+void myvector::set_coord(double x, double y) // можно перенести маленькие функции
 {
 	x_ = x;
 	y_ = y;
@@ -26,12 +26,12 @@ double myvector::get_y() const
 	return y_;
 }
 
-double vect_prod(myvector& v1, myvector& v2)
+double vect_prod(myvector& v1, myvector& v2) // не должна ли быть константной?
 {
 	return v1.get_x() * v2.get_x() + v1.get_y() * v2.get_y();
 }
 
-double angel(myvector& v1, myvector& v2)
+double angel(myvector& v1, myvector& v2) // то же
 {
-	return (vect_prod(v1, v2)) / (abs(v1.len()) * abs(v2.len()));
+	return (vect_prod(v1, v2)) / (abs(v1.len()) * abs(v2.len())); //проверка деления на 0?
 }
--- redactor/redactor/vector.h
+++ redactor/redactor/vector.h
@@ -5,10 +5,10 @@
 class myvector: public segment
 {
 public:
-	//Конструктор
+	//Конструктор                      //частный случай - нулевой вектор, что тогда?
 	myvector(point& begin,point& end);
 	//Установить координаты вектора
-	void set_coord(double x, double y);
+	void set_coord(double x, double y); //приватная
 	//Получить x координату
 	double get_x() const;
 	//Получить y координату
@@ -16,7 +16,7 @@ public:
 	//Деструктор
 	~myvector();
 	//Вычисление угла между векторами
-	friend double angel(myvector &v1,myvector &v2);
+	friend double angel(myvector &v1,myvector &v2); // входные данные меняются? если нет, то конст
 	//Вычисление скалярного произведения векторов
 	friend double vect_prod(myvector& v1, myvector& v2);
 
