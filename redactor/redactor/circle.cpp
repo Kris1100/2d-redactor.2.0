9e7a3c45cfea2fb2f3b93b9db5692ca9ade6908b
Kris1100 <bobr.200@yandex.ru>
2021-04-03 00:11:14 +0300

    В некоторых функциях вместо вывода возвращает значение(кроме пересечения с осями)

--- redactor/redactor/circle.cpp
+++ redactor/redactor/circle.cpp
@@ -24,11 +24,6 @@ istream& operator>>(istream& in, circle& c)
 	return in;
 }
 
-/*void circle::equation(double a, double b, double r) 
-{
-	circle(a, b, r);
-}
-*/
 
 double circle::get_r() const //можно перенести короткие функции в заголовочный файл
 {
@@ -51,33 +46,30 @@ point circle::get_p()
 }
 
 
-void circle::quarter() const //может, стоит переделать функцию так, чтобы она возвращала значение, а не печатала его
+int circle::quarter() const //может, стоит переделать функцию так, чтобы она возвращала значение, а не печатала его
                              //тогда она будет полезнее
 {
-	if ((_p->get_x() - _r) > 0 && (_p->get_y() - _r) > 0) //было слишком много в одной строке
-		//часть после уловия лучше переносить на следующую строчку
-		cout << "Окружность лежит в первой четверти" << endl;
-	else if ((_p->get_x() + _r) < 0 && (_p->get_y() - _r) > 0) 
-		cout << "Окружность лежит во второй четверти" << endl;
-	else if ((_p->get_x() + _r) < 0 && (_p->get_y() + _r) < 0) 
-		cout << "Окружность лежит в третьей четверти" << endl;
-	else if ((_p->get_x() - _r) > 0 && (_p->get_y() + _r) < 0) 
-		cout << "Окружность лежит в четвертой четверти" << endl;
-	else cout << "Окружность не лежит полностью ни в какой четверти" << endl;
+	if ((_p->get_x() - _r) > 0 && (_p->get_y() - _r) > 0) return 1;
+	else if ((_p->get_x() + _r) < 0 && (_p->get_y() - _r) > 0) return 2;
+	else if ((_p->get_x() + _r) < 0 && (_p->get_y() + _r) < 0) return 3;
+	else if ((_p->get_x() - _r) > 0 && (_p->get_y() + _r) < 0) return 4;
+	else return -1;
 }
 
 
 void circle::intersection() const 
 {    //не читабельный код - испрваить, также как в предыдущей
-	if (_r * _r - (_p->get_x() * _p->get_x()) > 0) cout << "Точка пересечения с осью x:(0, " << sqrt(_r * _r - (_p->get_x() * _p->get_x())) << ")  " << endl;
-	else if (_r * _r - (_p->get_y() * _p->get_y()) > 0) cout << "Точка пересечения с осью y:(" << sqrt(_r * _r - (_p->get_y() * _p->get_y())) << " ,0) " << endl;
+	if (_r * _r - (_p->get_x() * _p->get_x()) > 0)
+		cout << "Точка пересечения с осью x:(0, " << sqrt(_r * _r - (_p->get_x() * _p->get_x())) << ")  " << endl;
+	else if (_r * _r - (_p->get_y() * _p->get_y()) > 0) 
+		cout << "Точка пересечения с осью y:(" << sqrt(_r * _r - (_p->get_y() * _p->get_y())) << " ,0) " << endl;
 	else cout << "Нет пересечения с осями" << endl;
 }
 
 
-void circle::length() const // пусть возвращает число
+double circle::length() const 
 {
-	cout << "Длина окружности: " << (get_r() * PI * 2) << endl;
+	return (get_r() * PI * 2);
 }
 
 line circle::tangent(point& p) //входные данные меняются? если нет, то конст
