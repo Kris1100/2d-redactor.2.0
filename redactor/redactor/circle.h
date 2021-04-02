20bd76b5d690481d9e2229d572d23831ca41f8f7
Kris1100 <bobr.200@yandex.ru>
2021-04-03 00:10:04 +0300

    Структурирование функций по блокам

--- redactor/redactor/circle.h
+++ redactor/redactor/circle.h
@@ -8,34 +8,31 @@ using namespace std;
 
 class circle
 {
-public:
-	//Перезагрузка вывода                 //последовательность функций? разделение на смысловые блоки?
+public:	
+	//Конструктор окружности
+	circle(double x = 0.0, double y = 0.0, double r = 1.0);
+	//Перезагрузка вывода                
 	friend ostream& operator<<(ostream& out, const circle& c);
 	//Перезагрузка ввода
 	friend istream& operator>>(istream& in, circle& p);
 	//Возвращает диаметр
 	double get_d() const;
-	//Конструктор окружности
-	circle(double x = 0.0, double y = 0.0, double r = 1.0);
-	/*Задание через уравнение
-	void equation(double a = 0.0, double b = 0.0, double r = 1.0);
-	*/
 	//Возвращает радиус
 	double get_r()const;
-	//Сеттер радиуса
-	void set_r(double r = 1.0); //прриватная?
-	//Четверть, если окружность полностью в ней
-	void quarter() const;
+	// Возвращает центр
+	point get_p() const;
+	//Возвращает четверть, если окружность полностью в ней и -1 если лежит в окружности не полностью
+	int quarter() const;
 	//Возвращает длину окружности
-	void length() const;
+	double length() const;
 	//Точки пересечения с осями
 	void intersection() const; // может, тогда получить на вход точки и поменять их в функции, чтобы она была полезна
-
-	point get_p(); //конст?
 	//Возвращает касательную к окружности через данную точку
-	line tangent(point &p);
+	line tangent(point &p) ;
 	//деструктор?
 private:
 	point* _p;
 	double _r;
+	//Сеттер радиуса
+	void set_r(double r = 1.0); 
 };
