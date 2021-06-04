7fc2dbd514a9a664ce35ae34f5c04c454e20fa2d
alonmichu <alon.michu@gmail.com>
2021-06-04 14:51:03 +0300

    Added try-catch modules, and func is_inside in my_menu

--- redactor/redactor/Triangle.cpp
+++ redactor/redactor/Triangle.cpp
@@ -24,17 +24,21 @@ void triangle::set_vertex(const point& a, const point& b, const point& c)
 
 bool triangle::exists() const
 {
-	segment ab(vertex[0], vertex[1]);
-	segment bc(vertex[1], vertex[2]);
-	segment ac(vertex[0], vertex[2]);
-	if (ab.len() + bc.len() < ac.len() || ab.len() + ac.len() < bc.len() ||
-		ac.len() + bc.len() < ab.len())
+	if (vertex[0] == vertex[1] || vertex[1] == vertex[2] || vertex[2] == vertex[0])
+		return false;
+	line l(vertex[0], vertex[1]);
+	double a, b, c;
+	l.coef(a, b, c);
+	double x = vertex[2].get_x(), y = vertex[2].get_y();
+	if (abs(a * x + b * y + c) <= 0.00000001)
 		return false;
 	return true;
 }
 
 bool triangle::is_equilateral() const
 {
+	if (vertex[0] == vertex[1] || vertex[1] == vertex[2] || vertex[2] == vertex[0])
+		throw "error";
 	segment ab(vertex[0], vertex[1]);
 	segment bc(vertex[1], vertex[2]);
 	segment ac(vertex[0], vertex[2]);
@@ -46,6 +50,10 @@ bool triangle::is_equilateral() const
 
 bool triangle::is_isosceles() const
 {
+	if (vertex[0] == vertex[1] && vertex[1] == vertex[2])
+		throw "error";
+	if (vertex[0] == vertex[1] || vertex[1] == vertex[2] || vertex[2] == vertex[0])
+		return true;
 	segment ab(vertex[0], vertex[1]);
 	segment bc(vertex[1], vertex[2]);
 	segment ac(vertex[0], vertex[2]);
@@ -58,43 +66,44 @@ bool triangle::is_isosceles() const
 
 bool triangle::is_right() const
 {
+	if (!exists())
+		return false;
 	segment ab(vertex[0], vertex[1]);
 	segment bc(vertex[1], vertex[2]);
 	segment ac(vertex[0], vertex[2]);
-	if (is_equilateral())
-		return false;
 	if (ab.len() < bc.len())
 		swap(ab, bc);
 	if (ab.len() < ac.len())
 		swap(ab, ac);
 	double kat1 = ac.len(), kat2 = bc.len();
 	double hyp = ab.len();
-	if (abs(kat1 * kat1 + kat2 * kat2 - hyp * hyp) < 0.00001)
+	if (abs(kat1 * kat1 + kat2 * kat2 - hyp * hyp) < constants::eps)
 		return true;
 	return false;
 }
 
 double triangle::radius_inside() const
 {
-	if (exists())
-		return 2 * area() / perimetr();
-	throw "����������� ������";
+	if (!exists())
+		throw "Вырожденный случай";
+	return 2 * area() / perimetr();
 }
 
 double triangle::radius_outside() const
 {
-	if (exists())
-	{
-		double abc_square = area();
-		segment ab(vertex[0], vertex[1]);
-		segment bc(vertex[1], vertex[2]);
-		segment ac(vertex[0], vertex[2]);
-		return ab.len() * bc.len() * ac.len() / (4 * abc_square);
-	}
-	throw "����������� ������";
+	if (!exists())
+		throw "Вырожденный случай";
+	double abc_square = area();
+	segment ab(vertex[0], vertex[1]);
+	segment bc(vertex[1], vertex[2]);
+	segment ac(vertex[0], vertex[2]);
+	return ab.len() * bc.len() * ac.len() / (4 * abc_square);
 }
+
 bool triangle::is_inside(const point& p) const
 {
+	if (!exists())
+		return false;
 	double a, b, c;
 	int count = 0;
 	for (int i = 0; i < 3; i++)
@@ -137,7 +146,7 @@ point* triangle::get_vertex() const
 std::istream& operator>>(istream& in, triangle& t)
 {
 	double x, y;
-	cout << "������� ���������� " << 3 << " ������:" << endl;
+	cout << "Введите координаты " << 3 << " вершин:" << endl;
 	for (int i = 0; i < 3; i++)
 	{
 		cin >> x >> y;
@@ -173,7 +182,7 @@ void triangle::draw()
 	glEnd();
 }
 
-void triangle:: mymenu() 
+void triangle::mymenu()
 {
 	ifstream in("triangle.txt");
 	vector<string> commands;
@@ -188,7 +197,7 @@ void triangle:: mymenu()
 	SetColor(1, 15);
 	int item = 0;
 	print_inmenu(0, 1, commands);
-	if (not this->is_created) 
+	if (not this->is_created)
 	{
 		cin >> *this;
 		this->is_created = true;
@@ -204,109 +213,137 @@ void triangle:: mymenu()
 			case 0:
 			{
 				in.close();
-				cout << "������ ���������, ��������� � ������� ����" << endl;
+				cout << "Работа завершена, перейдите в главное меню" << endl;
 				return;
 			}
-			case 1: 
+			case 1:
 			{
 				cin >> *this;
+				roll_back_draw();
+				add_draw(*this);
 			}
 			break;
 			case 2:
 			{
-				if (this->is_equilateral())
-					cout << "����������� ��������������" << endl;
-				else
-					cout << "����������� �� �������� ��������������" << endl;
+				bool flag = false;
+				try
+				{
+					flag = is_equilateral();
+					if (flag)
+						cout << "Треугольник равносторонний" << endl;
+					else
+						cout << "Треугольник не является расносторонним" << endl;
+				}
+				catch (...)
+				{
+					cout << "Невозможно определить" << endl;
+				}
 			}
 			break;
 			case 3:
 			{
-				if (this->is_isosceles())
-					cout << "����������� ��������������" << endl;
-				else
-					cout << "����������� �� �������� ��������������" << endl;
+				bool flag = false;
+				try
+				{
+					flag = is_isosceles();
+					if (flag)
+						cout << "Треугольник равнобедренный" << endl;
+					else
+						cout << "Треугольник не является равнобедренным" << endl;
+				}
+				catch (...)
+				{
+					cout << "Невозможно определить" << endl;
+				}
 			}
 			break;
 			case 4:
 			{
-				if (this->is_right())
-					cout << "����������� �������������" << endl;
+				if (is_right())
+					cout << "Треугольник прямоугольный" << endl;
 				else
-					cout << "����������� �� �������� �������������" << endl;
+					cout << "Треугольник не является прямоугольным" << endl;
 			}
 			break;
 			case 5:
 			{
-				cout << "�������: " <<this->area() << endl;
+				cout << "Площадь: " << area() << endl;
 			}
 			break;
 			case 6:
 			{
-				cout << "��������: " << this->perimetr() << endl;
+				cout << "Периметр: " << perimetr() << endl;
 			}
 			break;
 			case 7:
 			{
 				double rad = 0;
-				cout << "������ ��������� ����������";
+				cout << "Радиус вписанной окружности";
 				try
 				{
-					rad = this->radius_inside();
+					rad = radius_inside();
 					cout << ": " << rad << endl;
 				}
 				catch (...)
 				{
-					cout << " ����������" << endl;
+					cout << " неопределён" << endl;
 				}
 			}
 			break;
 			case 8:
 			{
-				
 				double rad = 0;
 				cout << "Радиус описанной окружности";
 				try
 				{
-					rad = this->radius_outside();
+					rad = radius_outside();
 					cout << ": " << rad << endl;
 				}
 				catch (...)
 				{
-					cout << " ����������" << endl;
+					cout << " неопределён" << endl;
 				}
 			}
 			break;
-			break;
 			case 9:
 			{
-				if (not this->is_drawn) 
-				{
-					add_draw(*this);
-					cout << "������ ������� �������� � ������� �� ���������, �� ������� ���, ����� ��������� ������";
-					this->is_drawn = true;
-				}
-				else 
-				{
-					cout << "������ ��� � ������� �� ���������";
-				}
+				point p;
+				cin >> p;
+				if (is_inside(p))
+					cout << "Точка внутри треугольника" << endl;
+				else
+					cout << "Точка снаружи" << endl;
 			}
 			break;
 			case 10:
 			{
-				if (this->is_drawn) 
+				if (not this->is_drawn)
 				{
-					roll_back_draw();
-					this->is_drawn = false;
-					cout << "������ ������� ������ �� ������ �� ���������";
+					add_draw(*this);
+					cout << "Объект успешно добавлен в очередь на отрисовку, вы увидите его, когда завершите работу";
+					this->is_drawn = true;
 				}
-				else cout << "�� ��� �� ���������� ������";
+				else
+				{
+					cout << "Объект уже в очереди на отрисовку";
+				}
 			}
 			break;
 			case 11:
 			{
+				if (this->is_drawn)
+				{
+					roll_back_draw();
+					this->is_drawn = false;
+					cout << "Объект успешно удален из очерди на отрисовку";
+				}
+				else cout << "Вы еще не нарисовали объект";
+			}
+			break;
+			case 12:
+			{
 				roll_back_create();
-				cout << "������ ������� ������,��������� � ������� ����";
+				cout << "Объект успешно удален,перейдите в главное меню";
 				return;
 			}
 			break;
@@ -332,6 +369,7 @@ void triangle:: mymenu()
 			case 57: item = 9; break;
 			case 58: item = 10; break;
 			case 59: item = 11; break;
+			case 60: item = 12; break;
 			}
 			print_inmenu(item, 15, commands);
 			if (item < 0)
