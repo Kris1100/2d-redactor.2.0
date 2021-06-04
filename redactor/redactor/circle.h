56ba4e351cbf78eb573a103ad2a200c26b7a299b
alonmichu <alon.michu@gmail.com>
2021-06-05 00:55:22 +0300

    Improved draw func, cleaned code a bit

--- redactor/redactor/circle.h
+++ redactor/redactor/circle.h
@@ -3,12 +3,12 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <math.h>
 #include "point.h"
 #include "line.h"
 #include "math_const.h"
-#include "glut-3.7.6-bin/glut.h"
-#include <math.h>
 #include "Figure.h"
+#include "glut-3.7.6-bin/glut.h"
 using namespace std;
 
 class circle : public figure
@@ -18,10 +18,14 @@ public:
 	circle(double x = 0.0, double y = 0.0, double r = 1.0);
 	//Конструктор копирования
 	circle(const circle& c);
+	//Деструктор
+	~circle();
+
 	//Перезагрузка вывода                
 	friend ostream& operator<<(ostream& out, const circle& c);
 	//Перезагрузка ввода
 	friend istream& operator>>(istream& in, circle& p);
+
 	//Возвращает диаметр
 	double get_d() const {
 		return _r * 2;
@@ -34,6 +38,7 @@ public:
 	point get_p() const {
 		return _p;
 	}
+
 	//Возвращает четверть, если окружность полностью в ней и -1 если лежит в окружности не полностью
 	int quarter() const;
 	//Возвращает длину окружности
@@ -44,13 +49,11 @@ public:
 	void intersection() const; // может, тогда получить на вход точки и поменять их в функции, чтобы она была полезна
 	//Возвращает касательную к окружности через данную точку
 	line tangent(point &p);
-	//деструктор?
 
 	//Рисование окружности
 	void draw();
-
+	
 	void mymenu();
-
 
 private:
 	point _p;
@@ -59,5 +62,4 @@ private:
 	void set_r(double r = 1.0) {
 		_r = r;
 	}
-
 };
